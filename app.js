94d64bd4bb25a80c94f08279b7a57541d2a49ccf
Ali Hamza <hamza120025@gmail.com>
2024-02-25 16:58:49 +0000

    setup nodeJs server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my server!");
+});
+
+app.listen(port, () => {
+  //give url to user
+  console.log(`Server is running at http://localhost:${port}`);
+});
